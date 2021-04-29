c1837f1a9e54a6450221b94c819b5c66997c3ba1
Nicolas-Francisco <nicolas.garcia.r@ug.uchile.cl>
2021-04-28 23:41:47 -0400

    Se agrega la primera version del parse

--- Assignments/t1.rkt
+++ Assignments/t1.rkt
@@ -1,43 +1,115 @@
 #lang play
 
 ; <Prog>   ::= {<Fundef>* <Expr>}
-; Program structure
+; Program data structure
+; A program is composed of 0 or more FunDefs and a expression.
+; fedfs is a pair, which can contain fundefs or an empty pair.
 (deftype Prog
-  (f-defs fundef expr))
+  (program fdefs expr))
+
 
 ; <FunDef> ::= {define {<id> <id>*} <Expr>}
-; Function definition structure
+; Function definition data structure.
+; A function is composed of its name, 0 or more ids as argument and a expression
 (deftype FunDef
-  (emFunDef)
-  (d-fundef id rest-id expr))
+  (fundef fname args expr))
 
-#|
-<expr>   ::= <num>
+
+#|-----------------------------
+Expression abstract data type
+
+representation BNF:
+<Expr>   ::= <num>
            | <id>
            | <bool>           
-           | {<unop> <expr>}
-           | {<binop> <expr> <expr>}
-           | {if <expr> <expr> <expr>}
-           | {with {{<id> <expr>}*} <expr>}
-           | {<id> <expr>*}
+           | {<unop> <Expr>}
+           | {<binop> <Expr> <Expr>}
+           | {if <Expr> <Expr> <Expr>}
+           | {with {{<id> <Expr>}*} <Expr>}
+           | {<id> <Expr>*}
 |#
 (deftype Expr
-  (null)
   (num n)
   (id x)
   (bool b)
-  (un un-op expr)
-  (bin bi-op l r)
-  (if con t-branch f-branch)
-  (with id n-expr body)
-  (aExpr id expr))
+  (unop op expr)
+  (binop op l r)
+  (expr-if c t-branch f-branch)
+  (with id-list body)
+  (app id exprs))
 
+
+#|-----------------------------
+Environment abstract data type
+ 
+empty-env  :: Env
+extend-env :: Sym Val Env -> Env
+env-lookup :: Sym Env -> Val
+ 
+representation BNF:
+<env> ::= (mtEnv)
+        | (aEnv <id> <val> <env>)
+|#
 (deftype Env
   (mtEnv)
-  (aEnv id val env))
+  (aEnv id val rest-env))
+ 
+(def empty-env  (mtEnv))
+ 
+(def extend-env aEnv)
+ 
+(define (env-lookup x env)
+  (match env
+    [(mtEnv) (error 'env-lookup "free identifier: ~a" x)]
+    [(aEnv id val rest)
+     (if (symbol=? id x)
+         val
+         (env-lookup x rest))]))
 
-; <unop>   ::= ! | add1 | sub1  
-(deftype Unop)
 
-; <binop>  ::= + | - | * | / | && | = | < | ...
-(deftype Binop)
+; <Unop>   ::= ! | add1 | sub1
+; We can make parse for the unary op.
+(define (parse-unop un)
+  (match un
+    ['! not]
+    ['add1 add1]
+    ['sub1 sub1]))
+
+(define unops (list '! 'add1 'sub1))
+(define (is-unop? x) (member x unop))
+
+; <Binop>  ::= + | - | * | / | && | = | < | ...
+; && and || can be defined with a λ expression directly from the parse
+(define (parse-binop bin)
+  (match bin
+    ['+ +]
+    ['- -]
+    ['* *]
+    ['/ /]
+    ['= =]
+    ['< <]
+    ['> >]))
+
+(define binops (list '+ '- '* '/ '= '< '>))
+(define (is-binop? x) (member x binops))
+
+
+; parse ::= scr -> Expr
+; Parser function
+(define (parse src)
+  (match src
+    [(? number?) (num src)]
+    [(? symbol?) (id src)]
+    [(? bool?) (bool src)]
+    [(list (? is-unop? op) expr) (unop (parse-unop op) (parse expr))]
+    [(list (? is-binop? op) l r) (binop (parse-binop op) (parse l) (parse r))]
+    [(list '&& l r) (binop (λ (x y) (and x y)) (parse l) (parse r))]
+    [(list '|| l r) (binop (λ (x y) (or x y)) (parse l) (parse r))]
+    [(list 'if c t f) (expr-if (parse c) (parse t) (parse f))]
+    [(list 'with id-list b)
+     (with (list (match id-list
+             [(list id expr rest) ((parse id) (parse expr) (parse rest))]))
+           (parse b))]
+    [(list fname args) (app fname (parse args))]))
+
+
